512b3dfa319d7e3e00925b331b8ab1cc1e7710f7
ElliotHYLee <hylee101001@gmail.com>
2017-05-14 14:37:35 +0900

    ddd

--- Test01/src/TestMain.java
+++ Test01/src/TestMain.java
@@ -1,8 +1,14 @@
+import java.util.List;
+
+import components.xmltree.XMLTree;
+import components.xmltree.XMLTree1;
 
 public class TestMain {
 
 	public static void main(String[] args) {
-		//hi
+		XMLTree dd = new XMLTree1("data/foo.xml");
+		String a = dd.child(0).attributeNames().toString();
+		System.out.println(a.substring(1, a.length()-1));
 	}
 
 }
